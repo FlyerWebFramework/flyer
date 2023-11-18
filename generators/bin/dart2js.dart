112928715a90da0e75ee9717734b5e36358fa1a6
Martin Jablečník <martin.jablecnik@email.cz>
2023-11-18 15:15:49 +0100

    Add scripts for tranform dart functions into javascript libraries

---
+++ generators/bin/dart2js.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+Future<void> main(List<String> arguments) async {
+  var result = await Process.run('dart', ['compile', 'js', ...arguments]);
+  stdout.write(result.stdout);
+  stderr.write(result.stderr);
+
+  final functions = getFunctions(arguments.first);
+  var result2 = Process.run('sed', ['/^(function dartProgram() {\$/d; /^})();\$/d;', arguments.last]);
+  var file = File(arguments.last.replaceAll('.js', '-out.js'));
+
+  file.writeAsString((await result2).stdout + (await functions).join('\n'));
+}
+
+Future<List<String>> getFunctions(String file) async {
+  var result = await Process.run('grep', ['-e', '@JS', '-A1', file]);
+  var functions = result.stdout
+      .toString()
+      .split('--\n')
+      .map((e) => e.replaceAll('\n', '').split(')')[1].split('('))
+      .map(
+        (e) => MapEntry(
+          e.first.split(' ').last,
+          e.last.split(',').map((e) => e.split(' ').last.replaceAll(RegExp(r'[\]}]'), '')).toList(),
+        ),
+      )
+      .toList();
+
+  return functions.map((function) => '''
+export function ${function.key}(${function.value.join(', ')}) {
+  return A.${function.key}(${function.value.join(', ')})
+}
+''').toList();
+}
