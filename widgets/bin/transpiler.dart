e61578fb7f4d34fe28ade24f3754813113e3aa3d
Martin Jablečník <martin.jablecnik@email.cz>
2023-12-19 14:41:53 +0100

    Add more PointLine types

--- widgets/bin/transpiler.dart
+++ widgets/bin/transpiler.dart
@@ -11,35 +11,81 @@ Future<void> main(List<String> arguments) async {
   Transform(fileLines).process();
 }
 
+enum PointLineType {
+  observable,
+  computed,
+  ;
+
+  const PointLineType();
+
+  @override
+  toString() => "@$name";
+}
+
+class Scanner {
+  bool isScanning = false;
+  final StringBuffer _parts = StringBuffer();
+  PointLineType? _type;
+
+  startScanning(PointLineType type) {
+    isScanning = true;
+    _type = type;
+  }
+
+  stopScanning() {
+    isScanning = false;
+    _parts.clear();
+    _type = null;
+  }
+
+  scan(String line) {
+    _parts.write(line);
+  }
+
+  String transform() {
+    switch (_type) {
+      case PointLineType.observable:
+        final trimmedLine = _parts.toString();
+        print(trimmedLine);
+
+        final split = trimmedLine.split('=');
+        if (split.length != 2) throw "Error during parsing: $trimmedLine";
+        final variableName = split[0].trim().split(' ').last;
+        final variableValue = split[1].substring(0, split[1].length - 1).trim();
+        final transformed = "let $variableName = \$state($variableValue);";
+
+        return transformed;
+      case PointLineType.computed:
+        return "todo";
+      case null:
+        throw "Cannot transform when PointLineType is null";
+    }
+  }
+}
+
 class Transform {
   Transform(this.fileLines);
 
+  final scanner = Scanner();
   List<String> fileLines;
-  bool isScanning = false;
-  StringBuffer parts = StringBuffer();
-
-  startScanning() => isScanning = true;
-
-  stopScanning() => isScanning = false;
 
   isPointStartLine(String line) => line.substring(0, 1) == '@';
 
-  isPointEndLine(String line) => line.substring(line.length-1, line.length) == ';';
+  isPointEndLine(String line) => line.substring(line.length - 1, line.length) == ';';
 
   process() {
     for (String line in fileLines) {
       final String trimmedLine = line.trim();
       if (trimmedLine.isEmpty) continue;
 
-      if (isScanning) {
+      if (scanner.isScanning) {
         if (isPointEndLine(trimmedLine)) {
-          parts.write(trimmedLine);
-          printTransformedCode();
-          parts.clear();
-          stopScanning();
+          scanner.scan(trimmedLine);
+          print(scanner.transform());
+          scanner.stopScanning();
           print("");
         } else {
-          parts.write(trimmedLine);
+          scanner.scan(trimmedLine);
         }
       } else {
         findPoint(trimmedLine);
@@ -47,25 +93,19 @@ class Transform {
     }
   }
 
-  findPoint(String trimmedLine) {
-    if (isPointStartLine(trimmedLine)) {
-      if (trimmedLine.contains("@observable")) {
-        print(trimmedLine);
-        startScanning();
-      }
-    }
+  bool checkPointType(String line, PointLineType type) {
+    return line.contains(type.toString());
   }
 
-  printTransformedCode() {
-    final trimmedLine = parts.toString();
-    print("Original: $trimmedLine");
-
-    final split = trimmedLine.split('=');
-    if (split.length != 2) throw "Error during parsing: $trimmedLine";
-    final variableName = split[0].trim().split(' ').last;
-    final variableValue = split[1].substring(0, split[1].length - 1).trim();
-    final transformed = "let $variableName = \$state($variableValue);";
-
-    print("Transformed: $transformed");
+  findPoint(String trimmedLine) {
+    if (isPointStartLine(trimmedLine)) {
+      for (var type in PointLineType.values) {
+        if (checkPointType(trimmedLine, type)) {
+          print(trimmedLine);
+          scanner.startScanning(type);
+          break;
+        }
+      }
+    }
   }
 }
