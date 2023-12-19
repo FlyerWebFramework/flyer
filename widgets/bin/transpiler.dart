e9220b60c06341e552eb50b4124dede48761c1a6
Martin Jablečník <martin.jablecnik@email.cz>
2023-12-19 15:10:43 +0100

    Add transformed code stack

--- widgets/bin/transpiler.dart
+++ widgets/bin/transpiler.dart
@@ -8,7 +8,8 @@ Future<void> main(List<String> arguments) async {
 
   final fileLines = await File(filePath).readAsLines();
 
-  Transform(fileLines).process();
+  List<TransformedCode> transformedCode = Transform(fileLines).process();
+  transformedCode.forEach(print);
 }
 
 enum PointLineType {
@@ -42,70 +43,100 @@ class Scanner {
     _parts.write(line);
   }
 
-  String transform() {
+  TransformedCode transform() {
+    final trimmedLine = _parts.toString();
     switch (_type) {
       case PointLineType.observable:
-        final trimmedLine = _parts.toString();
-        print(trimmedLine);
-
         final split = trimmedLine.split('=');
         if (split.length != 2) throw "Error during parsing: $trimmedLine";
         final variableName = split[0].trim().split(' ').last;
         final variableValue = split[1].substring(0, split[1].length - 1).trim();
         final transformed = "let $variableName = \$state($variableValue);";
 
-        return transformed;
+        return TransformedCode(
+          type: _type!,
+          dart: trimmedLine,
+          javaScript: transformed,
+        );
       case PointLineType.computed:
-        return "todo";
+        return TransformedCode(
+          type: _type!,
+          dart: trimmedLine,
+          javaScript: "Not implemented yet..",
+        );
       case null:
         throw "Cannot transform when PointLineType is null";
     }
   }
 }
 
+class TransformedCode {
+  TransformedCode({
+    required this.type,
+    required this.dart,
+    required this.javaScript,
+  });
+
+  PointLineType type;
+  String dart;
+  String javaScript;
+
+  @override
+  String toString() {
+    int num = 70;
+    return """$type:
+${"=" * num}
+  Dart: $dart 
+${"-" * num}
+    JS: $javaScript
+${"=" * num}
+""";
+  }
+}
+
 class Transform {
   Transform(this.fileLines);
 
-  final scanner = Scanner();
-  List<String> fileLines;
+  final List<String> fileLines;
+  final List<TransformedCode> _stack = [];
+  final _scanner = Scanner();
 
-  isPointStartLine(String line) => line.substring(0, 1) == '@';
+  bool _isPointStartLine(String line) => line.substring(0, 1) == '@';
 
-  isPointEndLine(String line) => line.substring(line.length - 1, line.length) == ';';
+  bool _isPointEndLine(String line) => line.substring(line.length - 1, line.length) == ';';
 
-  process() {
-    for (String line in fileLines) {
-      final String trimmedLine = line.trim();
-      if (trimmedLine.isEmpty) continue;
-
-      if (scanner.isScanning) {
-        if (isPointEndLine(trimmedLine)) {
-          scanner.scan(trimmedLine);
-          print(scanner.transform());
-          scanner.stopScanning();
-          print("");
-        } else {
-          scanner.scan(trimmedLine);
-        }
-      } else {
-        findPoint(trimmedLine);
-      }
-    }
-  }
-
-  bool checkPointType(String line, PointLineType type) {
+  bool _checkPointType(String line, PointLineType type) {
     return line.contains(type.toString());
   }
 
-  findPoint(String trimmedLine) {
-    if (isPointStartLine(trimmedLine)) {
+  void _findPoint(String trimmedLine) {
+    if (_isPointStartLine(trimmedLine)) {
       for (var type in PointLineType.values) {
-        if (checkPointType(trimmedLine, type)) {
-          print(trimmedLine);
-          scanner.startScanning(type);
+        if (_checkPointType(trimmedLine, type)) {
+          _scanner.startScanning(type);
           break;
         }
       }
     }
+  }
+
+  List<TransformedCode> process() {
+    for (String line in fileLines) {
+      final String trimmedLine = line.trim();
+      if (trimmedLine.isEmpty) continue;
+
+      if (_scanner.isScanning) {
+        if (_isPointEndLine(trimmedLine)) {
+          _scanner.scan(trimmedLine);
+          _stack.add(_scanner.transform());
+          _scanner.stopScanning();
+        } else {
+          _scanner.scan(trimmedLine);
+        }
+      } else {
+        _findPoint(trimmedLine);
+      }
+    }
+    return _stack;
   }
 }
